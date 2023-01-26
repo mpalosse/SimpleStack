eb38a3f42d281062f5b5701a4983c82ee1821dac
Franck Silvestre <franck.silvestre@gmail.com>
2023-01-26 12:55:26 +0100

    Improve tests

--- src/test/java/org/example/SimpleStackTest.java
+++ src/test/java/org/example/SimpleStackTest.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test behaviour of a SimpleStack")
 class SimpleStackTest {
 
     @Test
+    @DisplayName("Test the state of a newly created slack")
     public void testCreateEmptyStack() { // Test case
 
         // When a freshly stack is created
@@ -18,6 +22,7 @@ class SimpleStackTest {
     }
 
     @Test
+    @DisplayName("Test the push of items")
     public void testPush() throws EmptyStackException {
 
         // Given an empty stack and an item
@@ -31,9 +36,22 @@ class SimpleStackTest {
         assertFalse(stack.isEmpty(), "The stack must be not empty");
         assertEquals(1, stack.getSize(),"The stack constains 1 item");
         assertSame( item, stack.peek(),"The pushed item is on top of the stack");
+
+        // Given a new item to add
+        Item item2 = new SimpleItem();
+
+        // When we add the new item
+        stack.push(item2);
+
+        // then...
+        assertFalse(stack.isEmpty(), "The stack must be not empty");
+        assertEquals(2, stack.getSize(),"The stack constains 2 items");
+        assertSame( item2, stack.peek(),"The pushed item is on top of the stack");
     }
 
     @Test
+    @Disabled
+    @DisplayName("Test limit when trying to pop an empty stack")
     public void testPopOnEmptyStack()  {
         // Given an empty stack
         Stack stack = new SimpleStack();
